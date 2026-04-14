45e1b6b5724e2442f50cc958c274be35e01347e3
vishalsinhrajawat129334 <vishalsinh.rajawat129334@marwadiuniversity.ac.in>
2026-04-14 10:19:13 +0530

    Delete db directory

--- db/__init__.py
+++
@@ -1,1 +0,0 @@
-from .mongo import users_collection, chats_collection, files_collection
--- db/mongo.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from config import MONGO_URI
-
-client = MongoClient(MONGO_URI)
-db = client["AiBot"]
-
-users_collection = db["users"]
-chats_collection = db["chats"]
-files_collection = db["files"]
